8b172e96e25787d905702c54b259497c3cdaa57f
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-08 13:21:42 -0500

    sintaxis lambda

---
+++ src/main/java/com/example/java8streamapilambdaexpression/lambda/Op.java
@@ -0,0 +1,6 @@
+package com.example.java8streamapilambdaexpression.lambda;
+
+public interface Op {
+
+    double calcular(double n1, double n2);
+}
---
+++ src/main/java/com/example/java8streamapilambdaexpression/lambda/Scopes.java
@@ -0,0 +1,23 @@
+package com.example.java8streamapilambdaexpression.lambda;
+
+public class Scopes {
+
+    public double probarLocalVariable() {
+
+        final double n3 = 3;
+        Op ope = new Op() {
+
+            @Override
+            public double calcular(double n1, double n2) {
+                return n1 + n2+ n3;
+            }
+        };
+        
+        return ope.calcular(1,1);
+    }
+
+    public static void main(String[] args) {
+        Scopes app = new Scopes();
+        System.out.println(app.probarLocalVariable());
+    }
+}
