f85f87c16774e39c49d670b9176932ea91028a6b
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-08 12:13:42 -0500

    java functional

---
+++ src/test/java/com/example/java8streamapilambdaexpression/Java8StreamApiLambdaExpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8streamapilambdaexpression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StreamApiLambdaExpressionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
