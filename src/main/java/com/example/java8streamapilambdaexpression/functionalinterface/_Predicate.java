c722c54d6242099323479cbbfea3ade8f8c62b91
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-10 18:39:01 -0500

    ejemplo de Predicate

--- src/main/java/com/example/java8streamapilambdaexpression/functionalinterface/_Predicate.java
+++ src/main/java/com/example/java8streamapilambdaexpression/functionalinterface/_Predicate.java
@@ -1,5 +1,7 @@
 package com.example.java8streamapilambdaexpression.functionalinterface;
 
+import java.util.function.Predicate;
+
 public class _Predicate {
     public static void main(String[] args) {
         System.out.println(
@@ -8,9 +10,34 @@ public class _Predicate {
         System.out.println(
                 isPhoneNumberValid("90963738392333")
         );
+
+        System.out.println("With Predicate");
+        System.out.println(
+                isPhoneNumberValidPredicate.test("07098780987")
+        );
+        System.out.println(
+                isPhoneNumberValidPredicate.test("90963738392333")
+        );
+
+        // Is phone number valid and contains number 3?
+        System.out.println(
+                "Is phone number valid and contains number 3? " +
+                isPhoneNumberValidPredicate.and(containsNumber3).test("90963738392333")
+        );
+        //Is phone number valid or contains number 3?
+        System.out.println(
+                "Is phone number valid or contains number 3? " +
+                isPhoneNumberValidPredicate.or(containsNumber3).test("07030000000")
+        );
     }
 
     static boolean isPhoneNumberValid(String phoneNumber){
         return phoneNumber.startsWith("07") && phoneNumber.length() == 11;
     }
+
+    static Predicate<String> isPhoneNumberValidPredicate = phoneNumber ->
+            phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+    static Predicate<String> containsNumber3 = phoneNumber ->
+            phoneNumber.contains("3");
 }
